2a40d6bd2d272ba480984479dc62f7da8c6fe47e
Maksim <kazak.ya.maxim@gmail.com>
2021-09-15 21:20:27 +0300

    initial

---
+++ Maksim_Kazak/task_1_1.py
@@ -0,0 +1,5 @@
+some_string = str(input('Input some string to calculate len:\n'))
+string_len = 0
+for symbol in some_string:
+    string_len += 1
+print(f'The length of the string is: {string_len}')
